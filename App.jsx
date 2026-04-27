1b3ddbeaf66fdcd1cd726467f44d5a4917e0cdea
Rafa <rafumojica@gmail.com>
2026-04-27 14:31:14 -0700

    removed head

--- App.jsx
+++ App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
