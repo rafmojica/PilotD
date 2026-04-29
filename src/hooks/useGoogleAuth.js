ff603480d930b4ac28e775692c45bb5645a36fda
Rafa <rafumojica@gmail.com>
2026-04-29 16:50:34 -0700

    added support for mobile

--- src/hooks/useGoogleAuth.js
+++ src/hooks/useGoogleAuth.js
@@ -52,6 +52,7 @@ export const useGoogleAuth = () => {
 
   const [request, response, promptAsync] = Google.useAuthRequest({
     webClientId: process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID,
+    iosClientId: process.env.EXPO_PUBLIC_GOOGLE_IOS_CLIENT_ID,
   });
 
   // Native: handle the OAuth response from expo-auth-session
